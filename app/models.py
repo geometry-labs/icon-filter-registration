58f10d75389927eee7158f4d1c09239f323f4252
Richard Mah <dick.mah@gmail.com>
2021-02-03 10:16:23 -0500

    Refactor models for new transaction class

--- app/models.py
+++ app/models.py
@@ -1,14 +1,17 @@
 import re
+from typing import Any, Dict, Optional, Type
 
 from pydantic import BaseModel, validator
 
+from app.settings import Settings
 
-class LogSubject(BaseModel):
+
+class LogEventRegistration(BaseModel):
     address: str
     keyword: str
 
     @validator("address")
-    def starts_with_address_indicator(cls, v):
+    def has_contract_prefix(cls, v):
         if not re.search("^cx", v):
             raise ValueError("Contract addresses must start with 'cx'")
         return v
@@ -27,14 +30,53 @@ class LogSubject(BaseModel):
             )
         return v
 
-
-class LogEventRegistration(BaseModel):
-    subject: LogSubject
     position: int
     active: bool
 
 
-class LogEventRegistrationMessage(BaseModel):
-    address: str
+class TransactionRegistration(BaseModel):
+    from_address: Optional[str]
+    to_address: Optional[str]
+    value: Optional[float]
+    active: Optional[bool]
+
+    @validator("from_address", "to_address")
+    def has_address_prefix(cls, v):
+        if not re.search("^cx|^hx", v):
+            raise ValueError("ICON addresses must start with 'hx' or 'cx'")
+        return v
+
+    @validator("from_address", "to_address")
+    def is_correct_length(cls, v):
+        if len(v) != 42:
+            raise ValueError("ICON addresses must be 42 characters in length")
+        return v
+
+
+class RegistrationMessage(BaseModel):
+    type: str
+    from_address: Optional[str]
+    to_address: Optional[str]
+    value: Optional[float]
+    keyword: Optional[str]
+    position: Optional[int]
+
+    class Config:
+        @staticmethod
+        def schema_extra(
+            schema: Dict[str, Any], model: Type["RegistrationMessage"]
+        ) -> None:
+            for prop in schema.get("properties", {}).values():
+                prop.pop("title", None)
+            schema["title"] = Settings().registrations_topic + "-value"
+
+
+class TransactionRegistrationMessage(RegistrationMessage):
+    type = "trans"
+
+
+class LogEventRegistrationMessage(RegistrationMessage):
+    type = "logevent"
+    to_address: str
     keyword: str
     position: int
