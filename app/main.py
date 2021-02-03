321a2d55e1bbb8f2b19f800882b25bec49a3283c
Richard Mah <dick.mah@gmail.com>
2021-02-03 10:13:20 -0500

    Move settings from main to new module

--- app/main.py
+++ app/main.py
@@ -11,23 +11,8 @@ from confluent_kafka.serialization import (
     StringSerializer,
 )
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseSettings, Field
 
-from app.models import LogEventRegistration, LogEventRegistrationMessage, LogSubject
-
-
-class Settings(BaseSettings):
-    kafka_server: str = Field(..., env="registration_kafka_server")
-    schema_server: str = Field(..., env="registration_schema_server")
-    kafka_compression: str = Field("gzip", env="registration_kafka_compression")
-    registrations_topic: str = Field(
-        "registrations", env="registration_registrations_topic"
-    )
-    db_server: str = Field(..., env="registration_db_server")
-    db_port: int = Field(5432, env="registration_db_port")
-    db_user: str = Field(..., env="registration_db_user")
-    db_password: str = Field(..., env="registration_db_password")
-    db_database: str = Field("postgres", env="registration_db_database")
+from app.settings import Settings
 
 
 s = Settings()
