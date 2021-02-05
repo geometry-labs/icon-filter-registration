3e95a597baca671dc540f899e8849e5091a5d01e
Richard Mah <dick.mah@gmail.com>
2021-02-05 10:56:04 -0500

    Move utility functions to new file

---
+++ app/utils.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+
+from app.sql.base import SessionLocal
+
+
+def nonestr(s):
+    if s is None:
+        return "None"
+    return str(s)
+
+
+def acked(err, msg):
+    """
+    Kafka production error callback. Used to raise HTTP 500 when message production fails.
+    :param err: Kakfa error
+    :param msg: Kafka error message
+    :return: None
+    """
+    if err is not None:
+        raise HTTPException(
+            500, "Failed to deliver message: %s: %s" % (str(msg), str(err))
+        )
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
