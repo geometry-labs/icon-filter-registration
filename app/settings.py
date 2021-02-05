b09bc01353bb51d99fbb68c31107b6d3a977fc84
Richard Mah <dick.mah@gmail.com>
2021-02-05 10:57:42 -0500

    Update settings

--- app/settings.py
+++ app/settings.py
@@ -6,10 +6,13 @@ class Settings(BaseSettings):
     schema_server: str = Field(..., env="registration_schema_server")
     kafka_compression: str = Field("gzip", env="registration_kafka_compression")
     registrations_topic: str = Field(
-        "registrations", env="registration_registrations_topic"
+        "event_registrations", env="registration_registrations_topic"
     )
     db_server: str = Field(..., env="registration_db_server")
     db_port: int = Field(5432, env="registration_db_port")
     db_user: str = Field(..., env="registration_db_user")
     db_password: str = Field(..., env="registration_db_password")
     db_database: str = Field("postgres", env="registration_db_database")
+
+
+settings = Settings()
