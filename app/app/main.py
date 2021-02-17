56a9e85b78a96ef5fc8ba2c1c77cf79a285d95b1
rob <robcannonxyz@gmail.com>
2021-02-16 22:37:34 -0700

    adding docs and api endpoint prefixes with docs metadata

--- app/app/main.py
+++ app/app/main.py
@@ -1,10 +1,36 @@
 from fastapi import FastAPI
 
 from .routers import broadcaster, id, log_event, transaction
+from .settings import settings
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "broadcaster",
+        "description": "Get broadcaster data.",
+    },
+    {
+        "name": "modify",
+        "description": "Get modify data.",
+    },
+    {
+        "name": "id",
+        "description": "Get event log data.",
+    },
+    {"name": "log_event"},
+]
 
-app.include_router(log_event.router)
-app.include_router(transaction.router)
-app.include_router(broadcaster.router)
-app.include_router(id.router)
+
+app = FastAPI(
+    title="ICON Blockchain Event Registration REST API",
+    description="Register events of type block, trnsaction, and event log "
+    "with a broadcaster ... .",  # TODO add docs
+    version="v0.1.0",
+    openapi_tags=tags_metadata,
+    openapi_url=f"{settings.DOCS_ENDPOINT_PREFIX}/openapi.json",
+    docs_url=f"{settings.DOCS_ENDPOINT_PREFIX}",
+)
+
+app.include_router(log_event.router, prefix=settings.API_ENDPOINT_PREFIX)
+app.include_router(transaction.router, prefix=settings.API_ENDPOINT_PREFIX)
+app.include_router(broadcaster.router, prefix=settings.API_ENDPOINT_PREFIX)
+app.include_router(id.router, prefix=settings.API_ENDPOINT_PREFIX)
--- app/app/settings.py
+++ app/app/settings.py
@@ -2,6 +2,9 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    API_ENDPOINT_PREFIX: str = "/api/v1"
+    DOCS_ENDPOINT_PREFIX: str = "/api/v1/docs"
+
     KAFKA_SERVER: str
     SCHEMA_SERVER: str
     KAFKA_COMPRESSION: str = "gzip"
